8854aaa103b769dae608d623f0d97912cecebceb
Sumon Ahmed <bdsumon4u@gmail.com>
2022-07-05 22:12:57 +0600

    WebView With Cookie

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -2,14 +2,32 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import { WebView } from 'react-native-webview';
+import CookieManager from '@react-native-cookies/cookies';
+
+Date.prototype.addMonths = function(m) {
+  let date = new Date(this);
+  let years = Math.floor(m / 12);
+  let months = m - (years * 12);
+  if (years) date.setFullYear(date.getFullYear() + years);
+  if (months) date.setMonth(date.getMonth() + months);
+  return date;
+};
+
+// set a cookie
+CookieManager.set('http://islamic-qanda.com', {
+  name: 'TOKEN',
+  value: '1uLkusPNrvcuKF1Vj9UFxAt0hhb5wcJZD7Op04kl',
+  path: '/',
+  version: '1',
+  expires: new Date().addMonths(6).toString()
+}).then((done) => {
+  console.log('CookieManager.set =>', done);
+});
 
 export default function TabTwoScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
-    </View>
+    <WebView sharedCookiesEnabled={true} source={{ uri: 'http://islamic-qanda.com/dashboard' }} />
   );
 }
 
